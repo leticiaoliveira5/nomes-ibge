ace50911afc9a3fe7da0ffd662631e8df049c2f2
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-02-25 16:21:00 -0300

    refatora métodos com código duplicado

--- lib/api.rb
+++ lib/api.rb
@@ -5,22 +5,18 @@ API_NOMES_URL = 'https://servicodados.ibge.gov.br/api/v2/censos/nomes/'.freeze
 API_LOCALIDADES_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades/'.freeze
 
 class Api
-  def self.nomes(url)
-    response = Faraday.get(API_NOMES_URL + url)
-    return [] if response.status == 400
+  def self.nomes(input_nome)
+    response = Faraday.get(API_NOMES_URL + input_nome)
+    parser(response)
+  end
 
+  def self.localidades(tipo)
+    response = Faraday.get(API_LOCALIDADES_URL + tipo)
     parser(response)
   end
 
   def self.ranking_nomes(sexo, codigo_localidade)
     nomes("ranking?sexo=#{sexo}&localidade=#{codigo_localidade}")
-  end
-
-  def self.localidades(url)
-    response = Faraday.get(API_LOCALIDADES_URL + url)
-    return [] if response.status == 400
-
-    parser(response)
   end
 
   def self.estados
@@ -32,6 +28,8 @@ class Api
   end
 
   def self.parser(response)
+    return [] if response.nil? || response.status == 400
+
     JSON.parse(response.body, symbolize_names: true)
   end
 end
