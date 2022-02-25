1f0d47f4e60059ef8a643c862cb199a83d87ca5b
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-02-25 11:06:11 -0300

    Deleta models

--- models/municipio.rb
+++
@@ -1,7 +0,0 @@
-require 'active_record'
-
-class Municipio < ActiveRecord::Base
-  belongs_to :unidade_federativa
-  validates :nome, :sigla_uf, :codigo, :populacao, :unidade_federativa, presence: true
-  validates :codigo, uniqueness: true
-end
--- models/unidade_federativa.rb
+++
@@ -1,7 +0,0 @@
-require 'active_record'
-
-class UnidadeFederativa < ActiveRecord::Base
-  has_many :municipios
-  validates :nome, :sigla, :codigo, :populacao, presence: true
-  validates :nome, :sigla, :codigo, uniqueness: true
-end
