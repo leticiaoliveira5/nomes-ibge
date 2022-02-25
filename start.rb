66c5418c0a33eecac41e9563ce2e78d44641c7fd
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-02-25 13:13:54 -0300

    Faz o setup ao iniciar o app

--- start.rb
+++ start.rb
@@ -1,6 +1,10 @@
 require 'active_record'
 require 'csv'
+require_relative 'lib/view'
 
 # transforma arquivo csv em um array de hashes e salva no arquivo parsed_population_data.txt
 parsed_population_data = CSV.parse(File.read('data/populacao_2019.csv'), headers: true).map(&:to_h)
 File.write('data/parsed_population_data', parsed_population_data)
+
+puts '================== A aplicação está pronta ==================='
+View.bem_vindo
