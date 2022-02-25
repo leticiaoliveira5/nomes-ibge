18fd2b933af13bdf6c1d87ac8047fd2a1cb07895
leticiaoliveira5 <olivaleticia@hotmail.com>
2022-02-25 19:53:53 -0300

    Extrai os dados antes de rodar os testes

--- start.rb
+++ start.rb
@@ -1,10 +1,3 @@
-require 'csv'
 require_relative 'lib/view'
 
-# transforma arquivo csv em um array de hashes e salva no arquivo parsed_population_data.txt
-population_data = File.read('data/populacao_2019.csv')
-parsed_population_data = CSV.parse(population_data, headers: true).map(&:to_h)
-File.write('data/parsed_population_data', parsed_population_data)
-
-puts '================== A aplicação está pronta ==================='
 View.bem_vindo
