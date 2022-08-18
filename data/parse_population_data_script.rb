50937403794e3e0032633919501dc4d25f0db23b
Letícia Oliveira <74281572+leticiaoliveira5@users.noreply.github.com>
2022-08-18 18:33:47 -0300

    Fix eval issue (#16)
    * new ParseCSV class
    
    * removes eval
    
    * removes script
    
    * rubocop

--- data/parse_population_data_script.rb
+++
@@ -1,6 +0,0 @@
-require 'csv'
-
-# transforma arquivo csv em um array de hashes e salva no arquivo parsed_population_data.txt
-population_data = File.read('data/populacao_2019.csv')
-parsed_population_data = CSV.parse(population_data, headers: true).map(&:to_h)
-File.write('data/parsed_population_data', parsed_population_data)
