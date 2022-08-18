aecd1f6e936c91bc15b5772881d8f49c1a1d227d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-08-18 18:24:16 -0300

    rubocop

--- lib/parse_csv.rb
+++ lib/parse_csv.rb
@@ -1,11 +1,10 @@
 require 'csv'
 
 class ParseCSV
-# transforma arquivo csv em um array de hashes e salva no arquivo parsed_population_data.txt
+  # transforma arquivo csv em um array de hashes e salva no arquivo parsed_population_data.txt
 
   def self.call
     population_data = File.read('data/populacao_2019.csv')
-    parsed_population_data = CSV.parse(population_data, headers: true).map(&:to_h)
-    parsed_population_data
+    CSV.parse(population_data, headers: true).map(&:to_h)
   end
 end
